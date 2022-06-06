80318f494bb2cf9832f8619200c40c2f007e81e8
Iheb Chagra <ihebchagra@gmail.com>
2022-06-06 21:56:30 +0100

    add a lot of stuff

--- medicasearch/js/index.js
+++ medicasearch/js/index.js
@@ -1,16 +1,22 @@
 function search(keyword) {
 	if(keyword.length<1)
-		return
-	keyword = keyword.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+		return []
+	keyword = keyword.normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim();
 	var results = []
 	for(var i in data){
-		data_fields=Object.keys(data[i])
+		//data_fields=Object.keys(data[i])
+		data_fields=['DCI','Spécialité']
 		for(var u=0;u<data_fields.length;u++){
+			if(! Object.keys(data[i]).includes(data_fields[u])){
+				continue
+			}
 			var rel = getRelevance(data[i][data_fields[u]],keyword)
 			if(rel==0)
 				continue
 			results.push({relevance:rel,entry:data[i]})
 		}
+		if(results.length>100)
+			break
 	}
 	results.sort(compareRelevance)
 	for(i=0;i<results.length;i++){
@@ -46,20 +52,23 @@ function outputResult(results){
 	if(results.length == 0) {
 		var notice = document.createElement("div");
 		notice.id = "notice";
-		notice.innerHTML = "Aucun résultat";
+		notice.innerHTML = "Aucun résultat ,entrez le nom d'un DCI ou médicament et appuyez sur Recherche";
 		document.getElementById("results").appendChild(notice); 
 	} else {
 		for(var i in results){
 			var nom = document.createElement("h3");
 			nom.id = "nom";
-			nom.innerHTML = results[i]["Nom"];
+			nom.innerHTML = results[i]["Spécialité"] + ' ' + results[i]["Dosage"];
 			document.getElementById("results").appendChild(nom); 
-			data_fields=Object.keys(data[i])
+			data_fields=Object.keys(results[i])
 			for(var u=0;u<data_fields.length;u++){
-				if(data_fields[u]=="Nom")
+				if(data_fields[u]=='notice' || data_fields[u]=='rcp'){
+					var propriete = document.createElement("div");
+					propriete.id="propriete"
+					propriete.innerHTML = "- <b>" + data_fields[u] + "</b> : <a href='" + results[i][data_fields[u]] + "'>" + results[i][data_fields[u]] + "</a>";
+					document.getElementById("results").appendChild(propriete); 
 					continue
-				if(results[i][data_fields[u]] == undefined)
-					continue
+				}
 				var propriete = document.createElement("div");
 				propriete.id="propriete"
 				propriete.innerHTML = "- <b>" + data_fields[u] + "</b> : " + results[i][data_fields[u]];
