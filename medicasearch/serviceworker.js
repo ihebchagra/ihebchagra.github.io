e70277fa175e023c1e5dcb2439dcb283b6c25e1f
Iheb Chagra <51883835+ihebchagra@users.noreply.github.com>
2022-06-07 04:13:01 +0000

    Update serviceworker.js

--- medicasearch/serviceworker.js
+++ medicasearch/serviceworker.js
@@ -1,5 +1,5 @@
 // Use a cacheName for cache versioning
-var cacheName = 'v1:static';
+var cacheName = 'v2:static';
 
 // During the installation phase, you'll usually want to cache static assets.
 self.addEventListener('install', function(e) {
