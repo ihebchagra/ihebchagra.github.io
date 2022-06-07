9d22ecfe8d8b93d8840051284c8b183fc22b5064
Iheb Chagra <ihebchagra@gmail.com>
2022-06-07 04:57:49 +0100

    make results expandable

--- medicasearch/js/index.js
+++ medicasearch/js/index.js
@@ -15,7 +15,7 @@ function search(keyword) {
 				continue
 			results.push({relevance:rel,entry:data[i]})
 		}
-		if(results.length>100)
+		if(results.length>200)
 			break
 	}
 	results.sort(compareRelevance)
@@ -47,36 +47,82 @@ function compareRelevance(a, b) {
   return b.relevance - a.relevance
 }
 
+
+
+
+function hashCode(string){
+    var hash = 0;
+    for (var i = 0; i < string.length; i++) {
+        var code = string.charCodeAt(i);
+        hash = ((hash<<5)-hash)+code;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
+
+
 function outputResult(results){
 	document.getElementById("results").innerHTML="" 
 	if(results.length == 0) {
 		var notice = document.createElement("div");
 		notice.id = "notice";
-		notice.innerHTML = "Aucun résultat ,entrez le nom d'un DCI ou médicament et appuyez sur Recherche";
+		notice.innerHTML = "Aucun résultat, entrez le nom d'un DCI ou médicament et appuyez sur Recherche";
 		document.getElementById("results").appendChild(notice); 
 	} else {
 		for(var i in results){
+			data_fields=Object.keys(results[i])
+			resultstring=""
+			for(var u=0;u<data_fields.length;u++){
+				resultstring+=results[i][data_fields[u]]
+			}
+			var nameid= hashCode(resultstring);
+			if(document.getElementById(nameid) != null)
+				continue
+			if(data_fields.includes("Dosage"))
+				var name= results[i]["Spécialité"] + ' ' + results[i]["Dosage"];
+			else
+				var name= results[i]["Spécialité"];
+			var section = document.createElement("div");
+			section.id = nameid;
+			document.getElementById("results").appendChild(section); 
 			var nom = document.createElement("h3");
 			nom.id = "nom";
-			nom.innerHTML = results[i]["Spécialité"] + ' ' + results[i]["Dosage"];
-			document.getElementById("results").appendChild(nom); 
-			data_fields=Object.keys(results[i])
+			nom.innerHTML = name;
+			document.getElementById(nameid).appendChild(nom); 
+			var inc=0
 			for(var u=0;u<data_fields.length;u++){
+				inc+=1
 				if(data_fields[u]=='notice' || data_fields[u]=='rcp'){
 					var propriete = document.createElement("div");
 					propriete.id="propriete"
+					if(inc>8){
+						propriete.className="expandable"
+						propriete.hidden=true
+					}
 					propriete.innerHTML = "- <b>" + data_fields[u] + "</b> : <a href='" + results[i][data_fields[u]] + "'>" + results[i][data_fields[u]] + "</a>";
-					document.getElementById("results").appendChild(propriete); 
+					document.getElementById(nameid).appendChild(propriete); 
 					continue
 				}
 				var propriete = document.createElement("div");
 				propriete.id="propriete"
+				if(inc>8){
+					propriete.className="expandable"
+					propriete.hidden=true
+				}
 				propriete.innerHTML = "- <b>" + data_fields[u] + "</b> : " + results[i][data_fields[u]];
-				document.getElementById("results").appendChild(propriete); 
+				document.getElementById(nameid).appendChild(propriete); 
 				
 			}
+			var afficher = document.createElement("button");
+			afficher.id="afficher"
+			afficher.innerHTML = "<u>Afficher Plus>></u>";
+			afficher.setAttribute("onClick", "show("+ nameid +")")
+			document.getElementById(nameid).appendChild(afficher); 
+			
 			var divider = document.createElement("hr");
-			document.getElementById("results").appendChild(divider); 
+			document.getElementById(nameid).appendChild(divider); 
 		}
 	}
 }
@@ -84,6 +130,30 @@ function outputResult(results){
 function recherche(){
 	keyword = document.getElementById("query").value
 	outputResult(search(keyword))
+}
+
+function show(id){
+	parent = document.getElementById(id) 
+	parent.getElementsByTagName("button")[0].style.display = "none" 
+	hidden = parent.getElementsByClassName("expandable")
+	for(var i in hidden){
+		hidden[i].hidden = false
+	}
+}
+
+function showall(){
+	buttons = document.getElementsByTagName("button") 
+	for(var i in buttons){
+		if(isNaN(Number(i)))
+			continue
+		buttons[i].style.display = "none";
+	}
+	hidden = document.getElementsByClassName("expandable")
+	for(var i in hidden){
+		if(isNaN(Number(i)))
+			continue
+		hidden[i].hidden = false
+	}
 }
 
 
