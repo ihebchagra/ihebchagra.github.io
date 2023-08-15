97d3723c9168fb348d12f4950a3f544097ba3d28
Iheb Chagra <ihebchagra@gmail.com>
2023-08-15 12:41:03 +0100

    platforme

---
+++ platforme/prepatre_index.py
@@ -0,0 +1,21 @@
+import os
+
+def generate_index_html(directory='.'):
+    file_list = os.listdir(directory)
+    with open('index.html', 'w') as f:
+        f.write('<html>\n')
+        f.write('<head>\n')
+        f.write('<meta charset="UTF-8">\n')
+        f.write('</head>\n')
+        f.write('<body>\n')
+        f.write('<h1>Chagra\'s very professional platform</h1>\n')
+        f.write('<ul>\n')
+        for file_name in file_list:
+            if os.path.isfile(file_name) and file_name.endswith('.html'):
+                f.write('<li><a href="{}">{}</a></li>\n'.format(file_name, file_name[:-5]))
+        f.write('</ul>\n')
+        f.write('</body>\n')
+        f.write('</html>\n')
+
+generate_index_html()
+
---
+++ platforme/script.js
@@ -0,0 +1,20 @@
+// Get all elements with class "true"
+const trueDivs = document.querySelectorAll('.true');
+
+// Get all elements with class "false"
+const falseDivs = document.querySelectorAll('.false');
+
+// Add click event listener to trueDivs
+trueDivs.forEach(div => {
+  div.addEventListener('click', () => {
+    div.classList.toggle('clicked');
+  });
+});
+
+// Add click event listener to falseDivs
+falseDivs.forEach(div => {
+  div.addEventListener('click', () => {
+    div.classList.toggle('clicked');
+  });
+});
+
