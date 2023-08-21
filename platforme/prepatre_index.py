534c5d97e5239da6bdc812abedf38fe63cdb274a
Iheb Chagra <ihebchagra@gmail.com>
2023-08-21 10:11:24 +0100

    ecnmoins

--- platforme/prepatre_index.py
+++ platforme/prepatre_index.py
@@ -8,7 +8,7 @@ def generate_index_html(directory='.'):
         f.write('<meta charset="UTF-8">\n')
         f.write('</head>\n')
         f.write('<body>\n')
-        f.write('<h1>Chagra\'s very professional ECN platform</h1>\n')
+        f.write('<h1>ECNmoins</h1>\n')
         f.write('<ul>\n')
         for file_name in file_list:
             if os.path.isfile(file_name) and file_name.endswith('.html') and file_name!='index.html':
